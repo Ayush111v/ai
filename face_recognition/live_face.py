b0e82170b2584a10b5374328af9546c01e5bc67a
Ayush111v <pritimasuman09@gmail.com>
2026-05-16 11:14:23 +0530

    fixed nested repository

---
+++ face_recognition/live_face.py
@@ -0,0 +1,21 @@
+import face_recognition
+import cv2
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video_capture.read()
+
+    face_locations = face_recognition.face_locations(frame)
+
+    for top, right, bottom, left in face_locations:
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+
+    cv2.imshow("Live Face Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
+
